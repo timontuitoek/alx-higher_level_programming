9bf89a29e858805c063a510402d0f5c5ee7380a5
timontuitoek <tuitoektimon@gmail.com>
2023-08-10 15:05:33 -0700

    A program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print("{}".format(result))
+    from sys import argv
+
+    if (len(argv) < 2):
+        print("0")
+    elif (len(argv) == 2):
+        print(argv[1])
+    else:
+        dup = [int(argv[i]) for i in range(1, len(argv))]
+        Sum = sum(dup)
+        print(Sum)
