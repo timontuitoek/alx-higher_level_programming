bd96dd3fc943c10d5c6cf9ccac80095f74f56c05
timontuitoek <tuitoektimon@gmail.com>
2023-09-12 09:29:08 -0700

    A  function that reads a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,10 +5,9 @@ reads file and prints its contents
 
 
 def read_file(filename=""):
+    """
+    Replace 'my_file_0.txt' with the actual filename you want to read
+    """
     with open(filename, mode='r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
-
-""" Replace 'my_file_0.txt' with the actual filename you want to read"""
-read_file("my_file_0.txt")
-
