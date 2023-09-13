c8bca13b5a94432e60351c1d53ba3dfd1839b50f
timontuitoek <tuitoektimon@gmail.com>
2023-09-13 02:21:51 -0700

    A class Student that defines a student by: (based on 10-student.py)

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,26 +6,39 @@ Student to disk and reload
 
 class Student:
     """
-    Defining student first_name, last_name and age
+        Defines a student by first_name, last_name, and age.
     """
-def __init__(self, first_name, last_name, age):
 
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        """
+            Initializes a Student instance.
+            Args:
+                first_name (str): The first name of the student.
+                last_name (str): The last name of the student.
+                age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         """
-        Rtrieves a dictionary representation of a Student instance.
+            Retrieves a dictionary representation of a Student instance.
+            Args:
+                attrs (list, optional): List of attribute names to retrieve.
+                                        Defaults to None.
+            Returns:
+                dict: Dictionary representation of the Student instance.
         """
         if attrs is not None and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr)
-                    for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance.
+            Replaces all attributes of the Student instance.
+            Args:
+                json (dict): Dictionary containing attribute-value pairs.
         """
         for key, value in json.items():
             setattr(self, key, value)
