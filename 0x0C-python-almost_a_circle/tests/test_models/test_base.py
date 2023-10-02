701ac6433af3e9a4f5181651aed5755f744418dd
timontuitoek <tuitoektimon@gmail.com>
2023-10-02 05:39:00 -0700

    new-changes

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """Defines unittests for base.py.
-"""
 
+Unittest classes:
+    TestBase_instantiation - line 21
+    TestBase_to_json_string - line 108
+    TestBase_save_to_file - line 154
+    TestBase_from_json_string - line 232
+    TestBase_create - line 286
+    TestBase_load_from_file - line 338
+    TestBase_save_to_file_csv - line 404
+    TestBase_load_from_file_csv - line 482
+"""
 import os
 import unittest
 from models.base import Base
@@ -538,3 +547,4 @@ class TestBase_load_from_file_csv(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
