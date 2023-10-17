5a51d84f03e5b34701593f24e13494d3c3375f17
timontuitoek <tuitoektimon@gmail.com>
2023-10-17 14:08:00 -0700

    A script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- script displaying top 3 of cities temp during July and August
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
