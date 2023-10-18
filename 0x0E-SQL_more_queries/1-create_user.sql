6f60c8c807d0df6201ca9ae37797cfe67db16935
timontuitoek <tuitoektimon@gmail.com>
2023-10-18 06:11:37 -0700

    A script that creates the MySQL server user user_0d_1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- script that creates the MySql server user user_0d_1
+CREATE USER user_0d_1@localhost
+IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIBILEGES ON *.* TO user_0d_1@localhost WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+END IF;
