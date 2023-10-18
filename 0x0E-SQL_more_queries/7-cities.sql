eb592f03cc77531fdf256eacd9e829f58e6f6271
timontuitoek <tuitoektimon@gmail.com>
2023-10-18 08:54:54 -0700

    A script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
