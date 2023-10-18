815fe46e270757c594f9a0b3c6e168f949213a78
timontuitoek <tuitoektimon@gmail.com>
2023-10-18 12:19:25 -0700

    A script that lists all the cities of California that can be found in the database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- script that lists all ccities of california found in database
 -- You are not allowed to use the JOIN keyword
-SELECT id, name
-FROM cities
+SELECT id,
+name FROM cities,
 WHERE state_id = (
 	SELECT id FROM states
 	WHERE name = California )
