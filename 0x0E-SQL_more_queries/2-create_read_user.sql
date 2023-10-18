cd624bc2d64de44a885029fd1ad641d1b38c8812
timontuitoek <tuitoektimon@gmail.com>
2023-10-18 07:12:47 -0700

    A script that creates the database hbtn_0d_2 and the user user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- The user_0d_2 password should be set to user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@host IDENTIFIED BY "user_0d_2_pwd";
-GRANT SELECT ON btn_0d_2.* TO user_0d_2@host;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@host;
 FLUSH PRIVILEGES;
