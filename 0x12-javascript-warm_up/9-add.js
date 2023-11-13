07f32329194bf711254920d2072582048ff8ef42
timontuitoek <tuitoektimon@gmail.com>
2023-11-13 13:43:13 -0800

    A script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function add (c, d) {
+	return parseInt(c, 10) + parseInt(d, 10);
+}
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(add(arg1, arg2));
