b228c3c046b1f330b9ce7fec42474ebfe7b2cf85
timontuitoek <tuitoektimon@gmail.com>
2023-11-13 13:47:13 -0800

    A script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function factorial (n) {
+	if (isNaN(n) || n < 0) {
+		return 1;
+	}
+	else if (n === 0) {
+		return 1;
+	}
+	else {
+		return n * factorial(n - 1);
+	}
+}
+
+const arg = process.argv[2];
+console.log(factorial(parseInt(arg, 10)));
