aeec3d9a478771fcf5dd87cb6556e34a057203e5
timontuitoek <tuitoektimon@gmail.com>
2023-11-13 12:23:28 -0800

    A script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const cMessage = "C is fun";
+console.log(cMessage);
+
+const pythonMessage = "Pytho is fun";
+console.log(pythonMessage);
+
+const jsMessage = "JavaScript is amazing";
+console.log(jsMessage);
