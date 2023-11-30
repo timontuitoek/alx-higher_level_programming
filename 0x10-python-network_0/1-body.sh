ab0736a7cd93bd303e29eece99bfe901c9a5b97b
timontuitoek <tuitoektimon.com>
2023-12-01 00:17:43 +0300

    A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
- echo "Usage: $0 <URL>"
- exit 1
-fi
-
-url=$1
-response=$(curl -s -w "%{http_code}" "$url")
-status_code=${response: -3}
-body=${response::-3}
-
-if [ "$status_code" -eq 200 ]; then
- echo "$body"
-else
- echo "Request failed with status code $status_code"
-fi
+# GET request and display the body of the response
+curl -sL "$1"
