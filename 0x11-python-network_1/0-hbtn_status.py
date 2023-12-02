045dd20ab8a3078fa8b6b14d0997de0ceff6ff96
timontuitoek <tuitoektimon.com>
2023-12-02 20:33:17 +0300

    A  Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This module fetches the status of the URL https://alx-intranet.hbtn.io/status
+and displays the body of the response in a tabulated format.
+"""
+
+from urllib import request
+
+def fetch_status(url):
+    """
+    Fetches the status of the given URL and displays the body of the response.
+    
+    Args:
+    - url (str): The URL to fetch the status from.
+    """
+    with request.urlopen(url) as response:
+        content = response.read().decode('utf-8')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    fetch_status(url)
