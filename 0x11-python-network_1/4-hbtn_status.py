d7f1661c6b12a70e093a0e264a05cd203e600a05
timontuitoek <tuitoektimon.com>
2023-12-04 00:07:31 +0300

    changes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 """
-import requests
+This script takes a URL as input, sends a request to the URL, and displays the value of the variable X-Request-Id in the response header.
 """
 
 import requests
+import sys
 
-def get_status():
+def get_x_request_id(url):
+    """
+    Sends a request to the specified URL and retrieves the value of the X-Request-Id variable from the response header.
+
+    :param url: The URL to send the request to.
+    """
     try:
-        response = requests.get('https://alx-intranet.hbtn.io/status')
-        response.raise_for_status()
+        response = requests.get(url)
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(f"The value of X-Request-Id is: {x_request_id}")
+        else:
+            print("X-Request-Id not found in the response headers.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
-        data = response.json()
-        print('- Status')
-        for key, value in data.items():
-            print(f'\t- {key}: {value}')
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
 
-    except requests.exceptions.HTTPError as errh:
-        print(f'HTTP Error: {errh}')
-    except requests.exceptions.ConnectionError as errc:
-        print(f'Error Connecting: {errc}')
-    except requests.exceptions.Timeout as errt:
-        print(f'Timeout Error: {errt}')
-    except requests.exceptions.RequestException as err:
-        print(f'Error: {err}')
-
-if __name__ == '__main__':
-    get_status()
+    url = sys.argv[1]
+    get_x_request_id(url)
