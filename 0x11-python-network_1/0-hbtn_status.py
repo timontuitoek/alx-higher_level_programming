655d54f6aff223d858017b7104ac4289b9addc4e
timontuitoek <tuitoektimon.com>
2023-12-03 23:14:49 +0300

    A Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
 """
-This module fetches the status of the URL https://alx-intranet.hbtn.io/status
-and displays the body of the response in a tabulated format.
+Fetches the URL: https://alx-intranet.hbtn.io/status
 """
 
-
-from urllib import request, error
-
-
-def fetch_status(url):
-    """
-    Fetches the status of the given URL and displays the body of the response.
-    """
-    try:
-        with request.urlopen(url) as response:
-            content = response.read().decode('utf-8')
-
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-    except error.URLError as e:
-        print(f"Error fetching the URL: {e}")
+from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    fetch_status(url)
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = Request(url)
+
+    with urlopen(req) as response:
+        raw_content = response.read()
+        decoded_content = raw_content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(raw_content)))
+        print('\t- content: {_content}'.format(_content=raw_content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=decoded_content))
