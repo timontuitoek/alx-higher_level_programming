c463cd091491b5ca0636b8440fc77a793191958f
timontuitoek <tuitoektimon.com>
2023-12-03 23:57:17 +0300

    A Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,19 +9,14 @@ import sys
 
 def send_request_and_display_body(url):
     try:
-        # Send a request to the specified URL
         with urllib.request.urlopen(url) as response:
-            # Read and decode the response body
             response_body = response.read().decode('utf-8')
             print(response_body)
 
     except urllib.error.HTTPError as e:
-        # If an HTTP error occurs, print the error code
         print(f"Error code: {e.code}")
 
 if __name__ == "__main__":
-    # Get URL from command line arguments
     url = sys.argv[1]
 
-    # Call the function to send the request and display the body
     send_request_and_display_body(url)
