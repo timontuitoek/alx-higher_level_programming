03345db4592c8e9c3e5b45437a532524e84e3afa
timontuitoek <tuitoektimon.com>
2023-12-03 23:27:26 +0300

    A Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module takes a URL as a command-line argument, sends a request to the URL,
+This script takes in a URL, sends a request to the URL,
 and displays the value of the X-Request-Id variable found in the header of the response.
 """
 
@@ -8,21 +8,20 @@ and displays the value of the X-Request-Id variable found in the header of the r
 import urllib.request
 import sys
 
+def get_x_request_id():
+    """
+    Get the X-Request-Id of a given URL using urllib and sys.
+    """
+    url = sys.argv[1]
 
-def get_x_request_id(url):
-    try:
-        with urllib.request.urlopen(url) as response:
-            x_request_id = response.getheader('X-Request-Id')
-            if x_request_id:
-                print(f"The value of X-Request-Id is: {x_request_id}")
-            else:
-                print("X-Request-Id header not found in the response.")
-    except urllib.error.URLError as e:
-        print(f"Error connecting to the URL: {e}")
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        x_request_id = headers.get('X-Request-Id')
+
+        if x_request_id:
+            print(f"The value of X-Request-Id is: {x_request_id}")
+        else:
+            print("X-Request-Id header not found in the response.")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please provide a URL as a command-line argument.")
-    else:
-        url = sys.argv[1]
-        get_x_request_id(url)
+    get_x_request_id()
