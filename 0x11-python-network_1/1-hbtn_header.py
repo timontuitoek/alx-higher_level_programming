88548956a1fd802515cc19ad50c381bf791e0266
timontuitoek <tuitoektimon.com>
2023-12-03 23:18:39 +0300

    A Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-
 """
 This module takes a URL as a command-line argument, sends a request to the URL,
 and displays the value of the X-Request-Id variable found in the header of the response.
 """
 
+
 import urllib.request
 import sys
 
+
 def fetch_x_request_id(url):
     """
-    Fetches the value of the X-Request-Id variable from the header of the response.
-
-    Args:
-    - url (str): The URL to send the request to.
+    Fetches the value of the X-Request-Id variable from the header of the response
     """
-    req = urllib.request.Request(url)
-    
-    with urllib.request.urlopen(req) as response:
-        x_request_id = response.headers.get('X-Request-Id')
-    
-    if x_request_id:
-        print(x_request_id)
-    else:
-        print("X-Request-Id not found in the response header.")
+    try:
+        req = urllib.request.Request(url)
+        
+        with urllib.request.urlopen(req) as response:
+            x_request_id = response.headers.get('X-Request-Id')
+        
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("X-Request-Id not found in the response header.")
+    except urllib.error.URLError as e:
+        print(f"Error fetching the URL: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
