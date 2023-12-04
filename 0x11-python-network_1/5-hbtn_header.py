94001bf3f2cc3aa073e7da9566a67e29614979c1
timontuitoek <tuitoektimon.com>
2023-12-04 11:01:20 +0300

    changes

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,7 +9,11 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    response = requests.get(url)
 
+    x_request_id = response.headers.get('X-Request-Id')
 
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    if x_request_id is not None:
+        print(x_request_id)
+    else:
+        print("X-Request-Id not found in the response headers.")
