c17f25df66b6734d203aee303568c2745e9a3809
timontuitoek <tuitoektimon.com>
2023-12-04 10:21:40 +0300

    changes

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,10 +3,10 @@
 Import requests
 """
 
+
 if __name__ == '__main__':
     import requests
     from sys import argv
     payload = {'email': argv[2]}
     r = requests.post(argv[1], data=payload)
     print(r.text)
-    
