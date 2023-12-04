5413b1a2b9535ac27e656b942dc7526ae1cb725e
timontuitoek <tuitoektimon.com>
2023-12-04 10:20:41 +0300

    changes

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,13 +2,12 @@
 """_
 Importing files
 """
-
 import requests
 import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
     print(r.headers.get("X-Request-Id"))
-    
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
     payload = {'email': argv[2]}
     r = requests.post(argv[1], data=payload)
     print(r.text)
+    
