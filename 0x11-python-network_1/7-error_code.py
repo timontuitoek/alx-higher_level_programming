9b1d441417a8bf1c033574fa93cb276f1f05edee
timontuitoek <tuitoektimon.com>
2023-12-04 10:11:48 +0300

    A Python script that takes in a URL, sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,22 +3,11 @@
 Import requests
 '''
 
-import requests
-import sys
-
-def fetch_url_body(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-
-        print(response.text)
-
-    except requests.exceptions.HTTPError as e:
-        print(f"Error code: {e.response.status_code}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-
-# Example usage
-url_to_fetch = sys.argv[1]
-fetch_url_body(url_to_fetch)
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
