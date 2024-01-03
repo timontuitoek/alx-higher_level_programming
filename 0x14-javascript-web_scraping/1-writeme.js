5d247b153544ddcbffad8390763587f308dcec00
timontuitoek <tuitoektimon@gmail.com>
2024-01-04 01:39:31 +0300

    script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const fs = require("fs");
 
-// Get the file path and string from the command line arguments
 const filePath = process.argv[2];
-const content = process.argv[3];
+const contentToWrite = process.argv[3];
 
-fs.writeFile(filePath, content, "utf8", (err) => {
+fs.writeFile(filePath, contentToWrite, "utf-8", (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log("File written successfully");
   }
 });
